141e4dec2712ee86dfc37dbde3e965d25f9b3ffa
adityatuteja7 <90108990+adityatuteja7@users.noreply.github.com>
2023-09-23 17:12:56 +0530

    Update urls.py

--- OnlineFire_ReportingSystem/urls.py
+++ OnlineFire_ReportingSystem/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
 from firereport.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('reporting', reporting, name='reporting'),
@@ -30,4 +33,7 @@ urlpatterns = [
 
     path('logout/', Logout, name='logout'),
 
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
